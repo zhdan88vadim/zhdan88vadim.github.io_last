658230c810ee9b8c440e2308c94479db1aacb87c
vadimpirov85@gmail.com <vadimpirov85@gmail.com>
2016-11-09 23:18:55 +0300

    added site template

---
+++ new_chlw.org/sass/first/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $('#slide').bjqs({
+        // animtype: 'slide',
+        height: 400,
+        width: 660,
+        responsive: true,
+        randomstart: true,
+        automatic: false,
+        usecaptions: true,
+        animduration: 450,
+        animspeed: 4000,
+        showcontrols: false
+    });
+
+});
